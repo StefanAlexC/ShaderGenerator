0690413615cf3b6eabaf2f3db1f8c1ce7a592174
Alex <sc3316@ic.ac.uk>
2017-12-01 00:08:25 +0000

    Added Json Creation.

---
+++ src/main/java/Json/GLTypes.java
@@ -0,0 +1,8 @@
+package Json;
+
+public enum GLTypes {
+  glUniform2f,
+  glUniform3f,
+  glUniform4f,
+  glUniform1f,
+}
---
+++ src/main/java/Json/InputArg.java
@@ -0,0 +1,76 @@
+package Json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import java.util.List;
+
+public class InputArg {
+
+  //TODO: Extend to Arrays
+
+  /**
+   * resolution : {"func":"glUniform2f","args":[256,256]}
+   */
+
+  private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+  private final String name;
+  private final Values value;
+
+  public InputArg(String name, Values value) {
+    this.name = name;
+    this.value = value;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Values getValue() {
+    return value;
+  }
+
+  public String toJsonString() {
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.add("resolution", gson.toJsonTree(value));
+    String string = gson.toJson(jsonObject);
+    return string.substring(2, string.length() - 2);
+  }
+
+  public static String toStringListJson(List<InputArg> objects) {
+    return "{\n" + objects.stream().map(InputArg::toJsonString).reduce((x, y) -> x + ",\n" + y)
+        .orElse
+        ("") + "\n}";
+  }
+
+  public static class Values {
+
+    public Values(GLTypes func, List<Double> args) {
+      this.func = func;
+      this.args = args;
+    }
+
+    /**
+     * func : glUniform2f args : [256,256]
+     */
+
+    private GLTypes func;
+    private List<Double> args;
+
+    public GLTypes getFunc() {
+      return func;
+    }
+
+    public void setFunc(GLTypes func) {
+      this.func = func;
+    }
+
+    public List<Double> getArgs() {
+      return args;
+    }
+
+    public void setArgs(List<Double> args) {
+      this.args = args;
+    }
+  }
+}
